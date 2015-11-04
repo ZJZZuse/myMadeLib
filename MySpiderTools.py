33b8028d78936c4674988bda3a511d51867da75d
447338871@qq.com <447338871@qq.com>
2015-11-04 11:00:06 +0800

    company url2

--- MySpiderTools.py
+++ MySpiderTools.py
@@ -3,42 +3,26 @@ __author__ = 'Zhang'
 
 import urllib
 import sys
-import socket, ftplib
-from pyquery import PyQuery as pq
+import socket
 import myCommonToolsZ as zTools
 import html2text
 import os
+import mySpiderCfg
 
 '''
 common use for other file
 '''
+
 # pq = mySpiderTools.pq
 # acqHtml = mySpiderTools.tryGetHtml
 # myDecodeHtml = mySpiderTools.myDecodeHtml
 
+MySpiderCfg = mySpiderCfg.MySpiderCfg
 
 h = html2text.HTML2Text()
 h.ignore_links = True
 
 timeout = 10  # in seconds
-
-
-class MySpiderCfg:
-    baseUrl = 'http://yhyz.6000y.com'
-    mainActionUrl = 'http://yhyz.6000y.com/news/news_140_%d.html'
-    baseFilePath = 'F:/more/illusion/other/ling/other/text/aisirenText2/%s.txt'
-
-    def __init__(self, baseUrl, mainActionUrl, baseFilePath):
-        '''
-
-        :param baseUrl:
-        :param mainActionUrl:
-        :param baseFilePath:
-        :return:
-        '''
-        self.baseUrl = baseUrl
-        self.mainActionUrl = mainActionUrl
-        self.baseFilePath = baseFilePath
 
 
 def setdefaulttimeout(timeout):
@@ -54,11 +38,16 @@ def myDecodeHtml(content):
 
 def downLoadResource(fileName, url, goalTime=3, passIfExist=True):
     '''
-
-    :param fileName:
-    :param url:
-    :param goalTime:
-    :param passIfExist:
+    @Deprecated
+    @see thisModalName2
+    :param
+    fileName:
+    :param
+    url:
+    :param
+    goalTime:
+    :param
+    passIfExist:
     :return:
     '''
     if os.path.exists(fileName):
@@ -89,6 +78,13 @@ def downLoadResource(fileName, url, goalTime=3, passIfExist=True):
 
 
 def tryGetHtml(url, tryCount=3):
+    '''
+    @Deprecated
+    @see thisModalName2
+    :param url:
+    :param tryCount:
+    :return:
+    '''
     for i in range(0, tryCount):
 
         if i > 0:
@@ -105,6 +101,12 @@ tryAcqHtml = tryGetHtml
 
 
 def getHtml(url):
+    '''
+    @Deprecated
+    @see thisModalName2
+    :param url:
+    :return:
+    '''
     # page = urllib.urlopen(url,proxies={'http':"http://222.79.72.120:8090"})
     page = urllib.urlopen(url)
 
